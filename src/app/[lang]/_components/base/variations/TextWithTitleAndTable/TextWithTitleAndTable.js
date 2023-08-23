a02b75193b6fc83c3b02f8039259a39a042d178e
Grozdan Stoyanov <grozdanstoyanov@2create.studio>
2023-08-23 12:53:23 +0300

    Change styles if title is not present

--- src/app/[lang]/_components/base/variations/TextWithTitleAndTable/TextWithTitleAndTable.js
+++ src/app/[lang]/_components/base/variations/TextWithTitleAndTable/TextWithTitleAndTable.js
@@ -1,15 +1,22 @@
 import { PortableText } from "@portabletext/react";
+import clsx from "clsx";
 import Table from "./Table";
 
 const TextWithTitleAndTable = ({ data }) => {
 	const { table, title, body } = data;
 
 	return (
-		<div className="py-24">
+		<div className={clsx(title && "py-24", !title && "pt-6 pb-24")}>
 			<div className="text-center max-w-[83rem] mx-auto text-lg">
-				<h3 className="text-5xl font-bold">{title}</h3>
+				{title && <h3 className="text-5xl font-bold">{title}</h3>}
 
-				<div className="mt-12 max-w-[69rem] mx-auto [&>p+p]:mt-8">
+				<div
+					className={clsx(
+						"mt-12 mx-auto [&>p+p]:mt-8",
+						title && "max-w-[69rem]",
+						!title && "text-left max-w-[60rem] text-xl font-semibold"
+					)}
+				>
 					<PortableText value={body} />
 				</div>
 			</div>
