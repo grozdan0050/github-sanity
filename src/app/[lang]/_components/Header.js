6f70e5a9c5fb13cf155aaa275e573678395bfc64
Grozdan Stoyanov <grozdanstoyanov@2create.studio>
2023-08-24 15:25:43 +0300

    Add checks for header

--- src/app/[lang]/_components/Header.js
+++ src/app/[lang]/_components/Header.js
@@ -10,21 +10,30 @@ const Header = ({ header, settings }) => {
 					<SanityNextImage data={header.logo} />
 
 					<nav className="flex items-center">
-						<ul className="flex items-center mr-10 gap-6 font-semibold text-lg">
-							{header.navigation.map((item, index) => (
-								<li key={item._key + index}>
-									<Link href={item.link.url}>{item.link.text}</Link>
-								</li>
-							))}
-						</ul>
+						{header.navigation.length > 0 && (
+							<ul className="flex items-center mr-10 gap-6 font-semibold text-lg">
+								{header.navigation.map(
+									(item, index) =>
+										item._key &&
+										item.link.url &&
+										item.link.text && (
+											<li key={item._key + index}>
+												<Link href={item.link.url}>{item.link.text}</Link>
+											</li>
+										)
+								)}
+							</ul>
+						)}
 
-						<Link
-							href={settings.link.url}
-							target="_blank"
-							className="text-lg font-medium text-pink-500 bg-white px-5 py-2 rounded-full border-2 border-pink-500"
-						>
-							{settings.link.text}
-						</Link>
+						{settings.link.url && settings.link.text && (
+							<Link
+								href={settings.link.url}
+								target="_blank"
+								className="text-lg font-medium text-pink-500 bg-white px-5 py-2 rounded-full border-2 border-pink-500"
+							>
+								{settings.link.text}
+							</Link>
+						)}
 
 						<LanguageSwitcher />
 					</nav>
