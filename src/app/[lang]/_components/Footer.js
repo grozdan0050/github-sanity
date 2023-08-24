9a75600af525830e72c801f6aa0c133cc0d8d92e
Grozdan Stoyanov <grozdanstoyanov@2create.studio>
2023-08-24 15:31:00 +0300

    Add checks for footer

--- src/app/[lang]/_components/Footer.js
+++ src/app/[lang]/_components/Footer.js
@@ -11,41 +11,53 @@ const Footer = ({ footer }) => {
 			<div className="shell-sm">
 				<div className="flex flex-wrap py-10">
 					<div className="w-1/2 h-auto">
-						<SanityNextImage data={footer.logo} />
+						{footer.logo && <SanityNextImage data={footer.logo} />}
 
-						<div className="mt-10">
-							<PortableText value={footer.body} />
-						</div>
+						{footer.body && (
+							<div className="mt-10">
+								<PortableText value={footer.body} />
+							</div>
+						)}
 					</div>
 
 					<div className="w-1/2 h-auto">
-						<div className="flex items-center justify-end h-full">
-							<span className="inline-block">
-								{footer.chamberOfCommerceNumber}
-							</span>
-						</div>
+						{footer.chamberOfCommerceNumber && (
+							<div className="flex items-center justify-end h-full">
+								<span className="inline-block">
+									{footer.chamberOfCommerceNumber}
+								</span>
+							</div>
+						)}
 					</div>
 				</div>
 
 				<hr />
 
 				<nav className="flex flex-wrap items-center justify-between pt-4 pb-8">
-					<span className="font-medium">{footer.copyright}</span>
+					{footer.copyright && (
+						<span className="font-medium">{footer.copyright}</span>
+					)}
 
-					<ul className="flex flex-wrap gap-8 font-semibold">
-						{footer.navigation.map((item, index) => {
-							const {
-								_key,
-								link: { url, text },
-							} = item;
+					{footer.navigation.length > 0 && (
+						<ul className="flex flex-wrap gap-8 font-semibold">
+							{footer.navigation.map((item, index) => {
+								const {
+									_key,
+									link: { url, text },
+								} = item;
 
-							return (
-								<li key={_key + index}>
-									<Link href={url}>{text}</Link>
-								</li>
-							);
-						})}
-					</ul>
+								return (
+									_key &&
+									url &&
+									text && (
+										<li key={_key + index}>
+											<Link href={url}>{text}</Link>
+										</li>
+									)
+								);
+							})}
+						</ul>
+					)}
 				</nav>
 			</div>
 		</footer>
