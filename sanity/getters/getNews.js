bf4272cdc300468d49ba374e6796ce9dbee488f3
Grozdan Stoyanov <grozdanstoyanov@2create.studio>
2023-09-07 15:26:25 +0300

    Remove default value

--- sanity/getters/getNews.js
+++ sanity/getters/getNews.js
@@ -1,7 +1,7 @@
 import { getClient } from "../lib/getClient";
 import newsQuery from "../queries/newsQuery";
 
-const getNews = (preview = null, locale, isAllNews, numberOfNews) =>
+const getNews = (preview, locale, isAllNews, numberOfNews) =>
 	getClient(preview).fetch(newsQuery(!!locale, isAllNews, numberOfNews), {
 		locale: locale,
 	});
