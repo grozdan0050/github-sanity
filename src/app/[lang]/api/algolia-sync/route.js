b60fd210df37a93aa8153da6117b67cbd3fb8e20
Grozdan Stoyanov <grozdanstoyanov@2create.studio>
2023-09-08 16:21:52 +0300

    Update algolia api route

--- src/app/[lang]/api/algolia-sync/route.js
+++ src/app/[lang]/api/algolia-sync/route.js
@@ -1,30 +1,30 @@
 import algoliasearch from "algoliasearch/lite";
+import { redirect } from "next/navigation";
 import getNews from "../../../../../sanity/getters/getNews";
 
-export default async function POST(req, res) {
-	if (req.method === "POST") {
-		const algoliaClient = algoliasearch(
-			process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
-			process.env.ALGOLIA_ADMIN_API_KEY
-		);
+export async function GET(req, res) {
+	const algoliaClient = algoliasearch(
+		process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
+		process.env.ALGOLIA_ADMIN_API_KEY
+	);
 
-		const index = algoliaClient.initIndex("news");
+	const index = algoliaClient.initIndex("news");
 
-		const news = await getNews();
+	const news = await getNews(null, null);
 
-		const algoliaNews = news.map((newsItem) => {
-			return {
-				objectID: newsItem._id,
-				title: newsItem.title,
-				body: newsItem.body,
-				link: newsItem.link,
-				publishDate: newsItem.publishDate,
-			};
-		});
+	const algoliaNews = news.map((newsItem) => {
+		return {
+			objectID: newsItem._id,
+			title: newsItem.title,
+			body: newsItem.body,
+			link: newsItem.link,
+			publishDate: newsItem.publishDate,
+		};
+	});
 
-		await index.saveObjects(algoliaNews);
+	const algoliaResponse = await index.saveObjects(algoliaNews);
 
-		res.json(`Content successfully synchronized with Algolia search`);
-		res.end();
-	}
+	console.log(algoliaResponse);
+
+	redirect(`/`);
 }
